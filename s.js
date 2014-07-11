6c23866fca9ddf77c2f3f85fc762869f24c96e6e
s2 <s@ireoo.com>
2014-07-12 06:31:51 +0800

    更新主题程序

--- s.js
+++ s.js
@@ -15,7 +15,7 @@ var http = require('http'),
 server = http.createServer(function(req, res){
     req.setEncoding(encoding="utf8");
     var path = url.parse(req.url).pathname;
-    o.log(path);
+    o.log(path);   
 
     if(path == '/') path = '/index.html';
 
@@ -35,52 +35,97 @@ send404 = function(res){
 
 server.listen(8000);
 
-var users = [];
+var players = [];
+var s = [];
+var list = [];
+var videos = [];
 
 var io = require('socket.io')(server);
-//io.set('log level', 1);
-io.on('connection', function(socket){
-
-
-    socket.on('say everyone with room', function(msg){
+io.set('log level', 1);
+io.sockets.on('connection', function(socket){
+    socket.on('say to one',function(data){
         try
         {
-            o.log(socket.user.name + ' say: ' + msg);
-            io.to(socket.user.room).emit('say everyone with room', msg);
+            o.log(players[socket.id].name + ' to ' + players[data.tid].name + ' : ' + data.msg);
+            data.timer = (new Date()).getTime();
+            var d = data;
+            d.socketid = players[socket.id].name;
+            d.avatar = players[socket.id].avatar;
+            socket.emit('say to me', d);
+            var d = data;
+            d.id = players[data.tid].name;
+            d.lid = players[socket.id].name;
+            d.socketid = socket.id;
+            d.avatar = players[socket.id].avatar;
+            s[data.tid].emit('say to one', d);
+        }catch(e){o.log(e.stack);}
+    });
+    socket.on('say to everyone',function(data){
+        try
+        {
+            o.log(socket.user.name + ' say: ' + data.msg);
+            socket.broadcast.to(socket.user.room).emit('say to everyone', data);
 
         }catch(e){o.log(e.stack);}
     });
 
-    socket.on('say everyone with system', function(msg){
+    socket.on('sendboss', function(data){
         try
         {
 
-            o.log(socket.user.name + ' say: ' + msg);
-            io.emit('say everyone with room', msg);
+            o.log('主播进入，开始直播！');
+            socket.broadcast.emit('getboss', data);
+            o.log(data);
 
         }catch(e){o.log(e.stack);}
     });
-    socket.on('login', function(user) {
+
+    socket.on('system', function(data){
         try
         {
+            o.log('[system]: ' + data.msg);
+            if(data.all) {
+                io.emit('system', data);
+            }else{
+                io.to(socket.user.room).emit('system', data);
+            }
 
-            socket.emit('get users', users);
-            console.log('正在发送用户列表给新用户');
+
+        }catch(e){o.log(e.stack);}
+    });
+    socket.on('user connect', function(user) {
+        try
+        {
+            socket.join(user.room);
+            o.log('正在发送用户列表给新用户...');
+            socket.emit('get users', list);
+            o.log('用户列表发送完成!');
+            players[socket.id] = user;
+            s[socket.id] = socket;
+            //socket.emit('login', socket.id);
             socket.user = user;
-            users.push(user);
-            io.to(socket.user.room).emit('new user', user);
-            o.log('用户 ' + socket.user.name + ' 登录了！当前在线人数：' + String(users.length));
+            user.id = socket.id;
+            list.push(user);
 
+            socket.broadcast.emit('new user connect', user);
+            o.log('用户 ' + socket.user.name + ' 登录了！当前在线人数：' + String(list.length));
         }catch(e){o.log(e.stack);}
     });
     socket.on('disconnect', function(){
         try
         {
 
-            var user = ;
-            socket.broadcast.emit('change', players[socket.id]);
-
-            o.log('用户 ' + players[socket.id].name + ' 退出了! 当前在线人数：' + String(list.length));
+            console.log(socket);
+            var msg = '用户 ' + socket.user.name + ' 退出了! 当前在线人数：';
+            socket.broadcast.emit('user disconnect', socket.user);
+            players.splice(socket.id, 1);
+            s.splice(socket.id, 1);
+            for(var i=0; i < list.length; i++) {
+                if(list[i].id == socket.id) {
+                    list.splice(i, 1);
+                }
+            }
+            o.log(msg + String(list.length));
 
 
         }catch(e){o.log(e.stack);}
