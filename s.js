2e62fcaba40118198c9a0dc5a84d7e561c51f700
Ultra.o <s@ireoo.com>
2014-07-13 13:57:11 +0800

    更新服务端显示效果

--- s.js
+++ s.js
@@ -9,19 +9,20 @@
 var http = require('http'),
     url = require('url'),
     fs = require('fs'),
-    o  = require('util'),
     server;
 
 server = http.createServer(function(req, res){
     req.setEncoding(encoding="utf8");
     var path = url.parse(req.url).pathname;
-    o.log(path);   
+    //o.log(path);
 
-    if(path == '/') path = '/index.html';
+    //if(path == '/') path = '/index.html';
 
     fs.readFile(__dirname + path, function(err, data){
         if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': path.substr(path.length - 2) == 'js' ? 'text/javascript, charset=UTF-8' : 'text/html, charset=UTF-8'});
+        console.log('[' + __dirname + path + ']状态: ' + String(err));
+        res.writeHead(200, {'Content-Type': 'text/javascript, charset=UTF-8'});
+        //res.writeHead(200, {'Content-Type': path.substr(path.length - 2) == 'js' ? 'text/javascript, charset=UTF-8' : 'text/html, charset=UTF-8'});
         res.write(data, 'utf8');
         res.end();
     });
@@ -52,16 +53,16 @@ io.on('connection', function(socket){
             data.ip = socket.handshake.address.address;
             data.time = socket.handshake.time;
             socket.broadcast.to(socket.user.room).emit('say to everyone', data);
-            o.log(socket.user.name + ' say: ' + data.msg);
+            console.log(socket.user.name + ' say: ' + data.msg);
 
-        }catch(e){o.log(e.stack);}
+        }catch(e){console.log(e.stack);}
     });
 
     //系统信息
     socket.on('system', function(data){
         try
         {
-            o.log('[system]: ' + data.msg);
+            console.log('[system]: ' + data.msg);
             if(data.all) {
                 io.emit('system', data);
             }else{
@@ -69,7 +70,7 @@ io.on('connection', function(socket){
             }
 
 
-        }catch(e){o.log(e.stack);}
+        }catch(e){console.log(e.stack);}
     });
 
     //新用户登录
@@ -81,15 +82,15 @@ io.on('connection', function(socket){
 
             socket.join(user.room);
             socket.emit('get users', users);
-            o.log('用户列表发送完成!');
+            console.log('用户列表发送完成!');
             socket.user = user;
             user.id = socket.id;
             users.push(user);
             //console.log(io);
             socket.broadcast.emit('new user connect', user);
-            o.log('用户 ' + socket.user.name + ' 登录了！当前在线人数：' + String(socket.conn.server.clientsCount) + ', 使用的浏览器为: ' + socket.handshake.headers['user-agent']);
+            console.log('用户 ' + socket.user.name + ' 登录了！当前在线人数：' + String(socket.conn.server.clientsCount) + ', 使用的浏览器为: ' + socket.handshake.headers['user-agent']);
 
-        }catch(e){o.log(e.stack);}
+        }catch(e){console.log(e.stack);}
     });
 
     //用户离开
@@ -103,14 +104,14 @@ io.on('connection', function(socket){
                     users.splice(i, 1);
                 }
             }
-            o.log('用户 ' + socket.user.name + ' 退出了! 当前在线人数：' + String(socket.conn.server.clientsCount));
+            console.log('用户 ' + socket.user.name + ' 退出了! 当前在线人数：' + String(socket.conn.server.clientsCount));
 
-        }catch(e){o.log(e.stack);}
+        }catch(e){console.log(e.stack);}
     });
 });
 
 var webRTC = require('webrtc.io').listen(8001);
 
-o.log('服务器已经启动，开始监听8000端口!');
-o.log('视频服务器已经启动，开始监听8001端口!');
+console.log('服务器已经启动，开始监听8000端口!');
+console.log('视频服务器已经启动，开始监听8001端口!');
 
