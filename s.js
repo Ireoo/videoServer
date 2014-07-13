0b3a44c7633bc62481decbbc48987f5842c77bb6
Ultra.o <s@ireoo.com>
2014-07-13 14:14:11 +0800

    修正错误

--- s.js
+++ s.js
@@ -100,8 +100,8 @@ io.on('connection', function(socket){
         {
 
             socket.broadcast.emit('user disconnect', socket.user);
-            for(var i=0; i < list.length; i++) {
-                if(list[i].id == socket.id) {
+            for(var i=0; i < users.length; i++) {
+                if(users[i].id == socket.id) {
                     users.splice(i, 1);
                 }
             }
